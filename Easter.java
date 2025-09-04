6411033e42aafd6ab1e9ac7d8dfdd1dd0e37f98d
Broildler355 <fouadk2024@gmail.com>
2025-09-04 15:05:23 +0000

    update 1

--- Easter.java
+++ Easter.java
@@ -3,3 +3,13 @@ public static void main(String[] args) {
 
 }
 }
+int y=2025;
+int a=y%19;
+int b=y/100;
+int c=y%100;
+int d=b/4;
+int e=b%4;
+int f=(b+8)/25;
+int g=(b-f+1)/3;
+int h=(19*a+b-d-g+15)/30;
+int i=c/4;
