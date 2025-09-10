8687c101630334bd071730ebdeacb07dc7dedfe2
Broildler355 <fouadk2024@gmail.com>
2025-09-10 14:54:57 +0000

    Update 7

--- Easter.java
+++ Easter.java
@@ -14,14 +14,13 @@ int d=b/4;
 int e=b%4;
 int f=(b+8)/25;
 int g=(b-f+1)/3;
-int h=(19*a+b-d-g+15)/30;
+int h=(19*a+b-d-g+15)%30;
 int i=c/4;
 int k=c%4;
-int r = 31 + (2*e)+(2*i)-h-k;
+int r = (32 + (2*e)+(2*i)-h-k)%7;
 int m = (a+(11*h)+(22*r))/451;
-int n = (h+r-(7*m)+114)/32;
-int p = (h+r-(7*m)+114)%32;
-int q = p+1; 
+int n = (h+r-(7*m)+114)/31;
+int p = (h+r-(7*m)+114)%31;
 System.out.println("a="+a);
 System.out.println("b="+b);
 System.out.println("c="+c);
@@ -36,7 +35,6 @@ System.out.println("r="+r);
 System.out.println("m="+m);
 System.out.println("n="+n);
 System.out.println("p="+p);
-
-System.out.println("Therefore in " + y + ", Easter fell on " + n "\" + p+1 + ". [Month = n =" + n + " & day = p+1 = " + p + "]");
+System.out.println("Therefore in " + y + ", Easter fell on " + n +"/" + (++p) + ". Month -> n -> " + n + " & day -> p+1 -> " + p);
 }
 }
